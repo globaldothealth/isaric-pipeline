824597448101b6c7c1a14b798ab356fecd854df8
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-03-04 15:37:44 +0000

    Add Encounter draft Missing ability to do multi-nested data, so 'reason' and 'diagnosis' currently missing

---
+++ fhirflat/resources/encounter.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+from fhir.resources.encounter import Encounter as baseEncounter
+from .base import FHIRFlatBase
+import orjson
+
+from ..flat2fhir import expand_concepts
+from typing import TypeAlias, ClassVar
+
+JsonString: TypeAlias = str
+
+
+class Encounter(baseEncounter, FHIRFlatBase):
+
+    # attributes to exclude from the flat representation
+    flat_exclusions: ClassVar[set[str]] = FHIRFlatBase.flat_exclusions + (
+        "id",
+        "identifier",
+        "participant",  # participants other than the patient
+        "appointment",  # appointment that scheduled the encounter
+        "account",  # relates to billing
+        "dietPreference",
+        "specialArrangement",  # if translator, streatcher, wheelchair etc. needed
+        "specialCourtesy",  # contains ID information, VIP, board member, etc.
+    )
+
+    # required attributes that are not present in the FHIRflat representation
+    flat_defaults: ClassVar[list[str]] = FHIRFlatBase.flat_defaults + ["status"]
+
+    @classmethod
+    def cleanup(cls, data: JsonString) -> Encounter:
+        """
+        Load data into a dictionary-like structure, then
+        apply resource-specific changes and unpack flattened data
+        like codeableConcepts back into structured data.
+        """
+        data = orjson.loads(data)
+
+        for field in [
+            "subject",
+            "episodeOfCare",
+            "basedOn",
+            "careTeam",
+            "partOf",
+            "serviceProvider",
+            "admission.destination",
+            "admission.origin",
+        ]:
+            if field in data.keys():
+                data[field] = {"reference": data[field]}
+
+        # add default status back in
+        data["status"] = "completed"
+
+        data = expand_concepts(data)
+
+        # create lists for properties which are lists of FHIR types
+        for field in [x for x in data.keys() if x in cls.attr_lists()]:
+            data[field] = [data[field]]
+
+        return cls(**data)
---
+++ tests/test_encounter_resource.py
@@ -0,0 +1,258 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import os
+from fhirflat.resources.encounter import Encounter
+import datetime
+
+ENCOUNTER_DICT_INPUT = {
+    "resourceType": "Encounter",
+    "id": "f203",
+    "identifier": [{"use": "temp", "value": "Encounter_Roel_20130311"}],
+    "status": "completed",
+    "class": [
+        {
+            "coding": [
+                {
+                    "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+                    "code": "IMP",
+                    "display": "inpatient encounter",
+                }
+            ]
+        }
+    ],
+    "priority": {
+        "coding": [
+            {
+                "system": "http://snomed.info/sct",
+                "code": "394849002",
+                "display": "High priority",
+            }
+        ]
+    },
+    "type": [
+        {
+            "coding": [
+                {
+                    "system": "http://snomed.info/sct",
+                    "code": "183807002",
+                    "display": "Inpatient stay for nine days",
+                }
+            ]
+        }
+    ],
+    "subject": {"reference": "Patient/f201", "display": "Roel"},
+    "episodeOfCare": [{"reference": "EpisodeOfCare/example"}],
+    "basedOn": [{"reference": "ServiceRequest/myringotomy"}],
+    "partOf": {"reference": "Encounter/f203"},
+    "serviceProvider": {"reference": "Organization/2"},
+    "participant": [
+        {
+            "type": [
+                {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/v3-ParticipationType",  # noqa: E501
+                            "code": "PART",
+                        }
+                    ]
+                }
+            ],
+            "actor": {"reference": "Practitioner/f201"},
+        }
+    ],
+    "appointment": [{"reference": "Appointment/example"}],
+    "actualPeriod": {"start": "2013-03-11", "end": "2013-03-20"},
+    # "reason": [
+    #     {
+    #         "value": [
+    #             {
+    #                 "concept": {
+    #                     "text": "The patient seems to suffer from bilateral pneumonia and renal insufficiency, most likely due to chemotherapy."  # noqa: E501
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # ],
+    # "diagnosis": [
+    #     {
+    #         "condition": [{"reference": {"reference": "Condition/stroke"}}],
+    #         "use": [
+    #             {
+    #                 "coding": [
+    #                     {
+    #                         "system": "http://terminology.hl7.org/CodeSystem/diagnosis-role",  # noqa: E501
+    #                         "code": "AD",
+    #                         "display": "Admission diagnosis",
+    #                     }
+    #                 ]
+    #             }
+    #         ],
+    #     },
+    #     {
+    #         "condition": [{"reference": {"reference": "Condition/f201"}}],
+    #         "use": [
+    #             {
+    #                 "coding": [
+    #                     {
+    #                         "system": "http://terminology.hl7.org/CodeSystem/diagnosis-role",  # noqa: E501
+    #                         "code": "DD",
+    #                         "display": "Discharge diagnosis",
+    #                     }
+    #                 ]
+    #             }
+    #         ],
+    #     },
+    # ],
+    "account": [{"reference": "Account/example"}],
+    "dietPreference": [
+        {
+            "coding": [
+                {
+                    "system": "http://snomed.info/sct",
+                    "code": "276026009",
+                    "display": "Fluid balance regulation",
+                }
+            ]
+        }
+    ],
+    "specialArrangement": [
+        {
+            "coding": [
+                {
+                    "system": "http://terminology.hl7.org/CodeSystem/encounter-special-arrangements",  # noqa: E501
+                    "code": "wheel",
+                    "display": "Wheelchair",
+                }
+            ]
+        }
+    ],
+    "specialCourtesy": [
+        {
+            "coding": [
+                {
+                    "system": "http://terminology.hl7.org/CodeSystem/v3-EncounterSpecialCourtesy",  # noqa: E501
+                    "code": "NRM",
+                    "display": "normal courtesy",
+                }
+            ]
+        }
+    ],
+    "admission": {
+        "origin": {"reference": "Location/2"},
+        "admitSource": {
+            "coding": [
+                {
+                    "system": "http://snomed.info/sct",
+                    "code": "309902002",
+                    "display": "Clinical Oncology Department",
+                }
+            ]
+        },
+        "reAdmission": {"coding": [{"display": "readmitted"}]},
+        "destination": {"reference": "Location/2"},
+    },
+}
+
+ENCOUNTER_FLAT = {
+    "resourceType": "Encounter",
+    "class.code": "http://terminology.hl7.org/CodeSystem/v3-ActCode|IMP",
+    "class.text": "inpatient encounter",
+    "type.code": "http://snomed.info/sct|183807002",
+    "type.text": "Inpatient stay for nine days",
+    "episodeOfCare": "EpisodeOfCare/example",
+    "basedOn": "ServiceRequest/myringotomy",
+    "priority.code": "http://snomed.info/sct|394849002",
+    "priority.text": "High priority",
+    "subject": "Patient/f201",
+    "partOf": "Encounter/f203",
+    "serviceProvider": "Organization/2",
+    "actualPeriod.start": datetime.date(2013, 3, 11),
+    "actualPeriod.end": datetime.date(2013, 3, 20),
+    # "reason.value.concept.text": "The patient seems to suffer from bilateral pneumonia and renal insufficiency, most likely due to chemotherapy.",  # noqa: E501
+    # diagnosis.condition: ["Condition/stroke", "Condition/f201"],
+    # diagnosis.use.code: ["http://terminology.hl7.org/CodeSystem/diagnosis-role|AD", "http://terminology.hl7.org/CodeSystem/diagnosis-role|DD"],  # noqa: E501
+    "admission.origin": "Location/2",
+    "admission.admitSource.code": "http://snomed.info/sct|309902002",
+    "admission.admitSource.text": "Clinical Oncology Department",
+    "admission.reAdmission.code": [[]],
+    "admission.reAdmission.text": "readmitted",
+    "admission.destination": "Location/2",
+}
+
+ENCOUNTER_DICT_OUT = {
+    "resourceType": "Encounter",
+    "status": "completed",
+    "class": [
+        {
+            "coding": [
+                {
+                    "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+                    "code": "IMP",
+                    "display": "inpatient encounter",
+                }
+            ]
+        }
+    ],
+    "priority": {
+        "coding": [
+            {
+                "system": "http://snomed.info/sct",
+                "code": "394849002",
+                "display": "High priority",
+            }
+        ]
+    },
+    "type": [
+        {
+            "coding": [
+                {
+                    "system": "http://snomed.info/sct",
+                    "code": "183807002",
+                    "display": "Inpatient stay for nine days",
+                }
+            ]
+        }
+    ],
+    "subject": {"reference": "Patient/f201"},
+    "episodeOfCare": [{"reference": "EpisodeOfCare/example"}],
+    "basedOn": [{"reference": "ServiceRequest/myringotomy"}],
+    "partOf": {"reference": "Encounter/f203"},
+    "serviceProvider": {"reference": "Organization/2"},
+    "actualPeriod": {"start": "2013-03-11T00:00:00", "end": "2013-03-20T00:00:00"},
+    "admission": {
+        "origin": {"reference": "Location/2"},
+        "admitSource": {
+            "coding": [
+                {
+                    "system": "http://snomed.info/sct",
+                    "code": "309902002",
+                    "display": "Clinical Oncology Department",
+                }
+            ]
+        },
+        "reAdmission": {"coding": [{"display": "readmitted"}]},
+        "destination": {"reference": "Location/2"},
+    },
+}
+
+
+def test_encounter_to_flat():
+    bp = Encounter(**ENCOUNTER_DICT_INPUT)
+
+    bp.to_flat("test_encounter.parquet")
+
+    assert_frame_equal(
+        pd.read_parquet("test_encounter.parquet"),
+        pd.DataFrame(ENCOUNTER_FLAT, index=[0]),
+        # Date types are off otherwise, pyarrow uses pytz and pandas uses dateutil
+        check_dtype=False,
+    )
+    os.remove("test_encounter.parquet")
+
+
+def test_observation_from_flat():
+    visit = Encounter(**ENCOUNTER_DICT_OUT)
+
+    flat_visit = Encounter.from_flat("tests/data/encounter_flat.parquet")
+
+    assert visit == flat_visit
