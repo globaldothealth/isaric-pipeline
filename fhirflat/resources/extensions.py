8da59aa0a7e379103af2fcd323c7e251fbcf830e
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-03-20 17:08:28 -0700

    Update extensions, include relativePhase extension Gives start & end integers relative to admission

--- fhirflat/resources/extensions.py
+++ fhirflat/resources/extensions.py
@@ -1,20 +1,20 @@
+from __future__ import annotations
 from fhir.resources.extension import Extension as _Extension
 from fhir.resources.datatype import DataType as _DataType
-from fhir.resources.core import fhirabstractmodel
+from fhir.resources.fhirprimitiveextension import (
+    FHIRPrimitiveExtension as _FHIRPrimitiveExtension,
+)
 from fhir.resources import fhirtypes
-from pydantic.v1 import Field, root_validator, validator
-from pydantic.v1.error_wrappers import ErrorWrapper, ValidationError
-from pydantic.v1.errors import MissingError
-import typing
+from pydantic.v1 import Field, validator
 from typing import Union
 
 
-class eventPhase(_DataType):
+class timingPhase(_DataType):
 
-    resource_type = Field("eventPhaseExtension", const=True)
+    resource_type = Field("timingPhase", const=True)
 
     url: fhirtypes.Uri = Field(
-        "eventPhase",
+        "timingPhase",
         alias="url",
         title="identifies the meaning of the extension",
         description=(
@@ -97,6 +97,145 @@ class relativeDay(_DataType):
         ]
 
 
+class relativeStart(_DataType):
+
+    resource_type = Field("relativeStartExtension", const=True)
+
+    url: fhirtypes.Uri = Field(
+        "start",
+        alias="url",
+        title="identifies the meaning of the extension",
+        description=(
+            "Source of the definition for the extension code - a logical name or a "
+            "URL."
+        ),
+        # if property is element of this resource.
+        element_property=True,
+        element_required=True,
+    )
+
+    valueInteger: fhirtypes.Integer = Field(
+        None,
+        alias="valueInteger",
+        title="Value of extension",
+        description=(
+            "Value of extension - must be one of a constrained set of the data "
+            "types (see [Extensibility](extensibility.html) for a list)."
+        ),
+        # if property is element of this resource.
+        element_property=True,
+        element_required=True,
+    )
+
+    @classmethod
+    def elements_sequence(cls):
+        """returning all elements names from
+        ``Extension`` according specification,
+        with preserving original sequence order.
+        """
+        return [
+            "id",
+            "extension",
+            "url",
+            "valueInteger",
+        ]
+
+
+class relativeEnd(_DataType):
+
+    resource_type = Field("relativeEndExtension", const=True)
+
+    url: fhirtypes.Uri = Field(
+        "end",
+        alias="url",
+        title="identifies the meaning of the extension",
+        description=(
+            "Source of the definition for the extension code - a logical name or a "
+            "URL."
+        ),
+        # if property is element of this resource.
+        element_property=True,
+        element_required=True,
+    )
+
+    valueInteger: fhirtypes.Integer = Field(
+        None,
+        alias="valueInteger",
+        title="Value of extension",
+        description=(
+            "Value of extension - must be one of a constrained set of the data "
+            "types (see [Extensibility](extensibility.html) for a list)."
+        ),
+        # if property is element of this resource.
+        element_property=True,
+        element_required=True,
+    )
+
+    @classmethod
+    def elements_sequence(cls):
+        """returning all elements names from
+        ``Extension`` according specification,
+        with preserving original sequence order.
+        """
+        return [
+            "id",
+            "extension",
+            "url",
+            "valueInteger",
+        ]
+
+
+class relativePhase(_DataType):
+
+    resource_type = Field("relativePhase", const=True)
+
+    url: fhirtypes.Uri = Field(
+        "relativePhase",
+        alias="url",
+        title="identifies the meaning of the extension",
+        description=(
+            "Source of the definition for the extension code - a logical name or a "
+            "URL."
+        ),
+        # if property is element of this resource.
+        element_property=True,
+        element_required=True,
+    )
+
+    extension: list[Union[relativeStart, relativeEnd]] = Field(
+        None,
+        alias="extension",
+        title="List of `Extension` items (represented as `dict` in JSON)",
+        description="Additional content defined by implementations",
+        # if property is element of this resource.
+        element_property=True,
+        # this trys to match the type of the object to each of the union types
+        union_mode="smart",
+    )
+
+    @validator("extension")
+    def validate_extension_contents(cls, extensions):
+        start_count = sum(isinstance(item, relativeStart) for item in extensions)
+        end_count = sum(isinstance(item, relativeEnd) for item in extensions)
+
+        if start_count > 1 or end_count > 1:
+            raise ValueError("relativeStart and relativeEnd can only appear once.")
+
+        return extensions
+
+    @classmethod
+    def elements_sequence(cls):
+        """returning all elements names from
+        ``Extension`` according specification,
+        with preserving original sequence order.
+        """
+        return [
+            "id",
+            "extension",
+            "url",
+        ]
+
+
 class approximateDate(_DataType):
 
     resource_type = Field("approximateDateExtension", const=True)
@@ -141,7 +280,7 @@ class approximateDate(_DataType):
         ]
 
 
-class dateTimeExtension(fhirabstractmodel.FHIRAbstractModel):
+class dateTimeExtension(_FHIRPrimitiveExtension):
     """
     A G.Health specific extension to the FHIR dateTime type
     Allows dates to be specified as either approximate, and/or number of days relative
@@ -150,16 +289,7 @@ class dateTimeExtension(fhirabstractmodel.FHIRAbstractModel):
 
     resource_type = Field("dateTimeExtension", const=True)
 
-    id: fhirtypes.String = Field(
-        None,
-        alias="id",
-        title="Type `String`",
-        description="Unique id for inter-element referencing",
-        # if property is element of this resource.
-        element_property=False,
-    )
-
-    extension: typing.List[Union[approximateDate, relativeDay, _Extension]] = Field(
+    extension: list[Union[approximateDate, relativeDay, _Extension]] = Field(
         None,
         alias="extension",
         title="List of `Extension` items (represented as `dict` in JSON)",
@@ -182,52 +312,17 @@ class dateTimeExtension(fhirabstractmodel.FHIRAbstractModel):
 
         return extensions
 
-    @root_validator(pre=True)
-    def validate_extension_or_fhir_comment_required(
-        cls, values: typing.Dict[str, typing.Any]
-    ) -> typing.Dict[str, typing.Any]:
-        """Conditional Required Validation"""
-        errors = list()
-        extension = values.get("extension", None)
-        fhir_comments = values.get("fhir_comments", None)
 
-        if (
-            values.get("id", None) is None
-            and extension is None
-            and fhir_comments is None
-        ):
-            errors.append(ErrorWrapper(MissingError(), loc="extension"))
-            raise ValidationError(errors, cls)  # type: ignore
-
-        return values
-
-    @classmethod
-    def elements_sequence(cls):
-        """returning all elements names from ``FHIRPrimitiveExtension`` according
-        specification, with preserving original sequence order.
-        """
-        return ["id", "extension"]
-
-
-class phaseExtension(fhirabstractmodel.FHIRAbstractModel):
+class timingPhaseExtension(_FHIRPrimitiveExtension):
     """
     A G.Health specific extension to the FHIR dateTime type
     Allows dates to be specified as either approximate, and/or number of days relative
     to the current date.
     """
 
-    resource_type = Field("phaseExtension", const=True)
+    resource_type = Field("timingPhaseExtension", const=True)
 
-    id: fhirtypes.String = Field(
-        None,
-        alias="id",
-        title="Type `String`",
-        description="Unique id for inter-element referencing",
-        # if property is element of this resource.
-        element_property=False,
-    )
-
-    extension: typing.List[Union[eventPhase, _Extension]] = Field(
+    extension: list[Union[timingPhase, _Extension]] = Field(
         None,
         alias="extension",
         title="List of `Extension` items (represented as `dict` in JSON)",
@@ -240,35 +335,39 @@ class phaseExtension(fhirabstractmodel.FHIRAbstractModel):
 
     @validator("extension")
     def validate_extension_contents(cls, extensions):
-        phase_count = sum(isinstance(item, approximateDate) for item in extensions)
+        phase_count = sum(isinstance(item, timingPhase) for item in extensions)
 
         if phase_count > 1:
-            raise ValueError("eventPhase can only appear once.")
+            raise ValueError("timingPhase can only appear once.")
 
         return extensions
 
-    @root_validator(pre=True)
-    def validate_extension_or_fhir_comment_required(
-        cls, values: typing.Dict[str, typing.Any]
-    ) -> typing.Dict[str, typing.Any]:
-        """Conditional Required Validation"""
-        errors = list()
-        extension = values.get("extension", None)
-        fhir_comments = values.get("fhir_comments", None)
 
-        if (
-            values.get("id", None) is None
-            and extension is None
-            and fhir_comments is None
-        ):
-            errors.append(ErrorWrapper(MissingError(), loc="extension"))
-            raise ValidationError(errors, cls)  # type: ignore
+class relativePhaseExtension(_FHIRPrimitiveExtension):
+    """
+    A G.Health specific extension to the FHIR dateTime type
+    Allows dates to be specified as either approximate, and/or number of days relative
+    to the current date.
+    """
 
-        return values
+    resource_type = Field("relativePhaseExtension", const=True)
 
-    @classmethod
-    def elements_sequence(cls):
-        """returning all elements names from ``FHIRPrimitiveExtension`` according
-        specification, with preserving original sequence order.
-        """
-        return ["id", "extension"]
+    extension: list[Union[relativePhase, _Extension]] = Field(
+        None,
+        alias="extension",
+        title="List of `Extension` items (represented as `dict` in JSON)",
+        description="Additional content defined by implementations",
+        # if property is element of this resource.
+        element_property=True,
+        # this trys to match the type of the object to each of the union types
+        union_mode="smart",
+    )
+
+    @validator("extension")
+    def validate_extension_contents(cls, extensions):
+        phase_count = sum(isinstance(item, relativePhase) for item in extensions)
+
+        if phase_count > 1:
+            raise ValueError("relativePhase can only appear once.")
+
+        return extensions
