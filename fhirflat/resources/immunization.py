13684386785f3d71c906aa3e1324fb098da65976
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-03-21 13:36:03 -0700

    Add immunization extensions

--- fhirflat/resources/immunization.py
+++ fhirflat/resources/immunization.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from fhir.resources.immunization import Immunization as _Immunization
 from .base import FHIRFlatBase
+from .extensions import dateTimeExtension, timingPhaseExtension
+from pydantic.v1 import Field
 import orjson
 
 from ..flat2fhir import expand_concepts
@@ -10,6 +12,25 @@ JsonString: TypeAlias = str
 
 
 class Immunization(_Immunization, FHIRFlatBase):
+
+    extension: timingPhaseExtension = Field(
+        None,
+        alias="extension",
+        title="Additional content defined by implementations",
+        description=(
+            """
+            Contains the G.H 'eventPhase' extension, and allows extensions from other
+             implementations to be included."""
+        ),
+        # if property is element of this resource.
+        element_property=True,
+    )
+
+    occurrenceDateTime__ext: dateTimeExtension = Field(
+        None,
+        alias="_occurrenceDateTime",
+        title="Extension field for ``occurrenceDateTime``.",
+    )
 
     # attributes to exclude from the flat representation
     flat_exclusions: ClassVar[set[str]] = FHIRFlatBase.flat_exclusions + (
