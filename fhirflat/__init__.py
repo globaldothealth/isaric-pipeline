e35200347a903bdf1631cbfb342566bdecf19dcb
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-05-15 17:08:54 +0100

    Make fhirflat installable

---
+++ fhirflat/__init__.py
@@ -0,0 +1,14 @@
+from .resources.condition import Condition
+from .resources.encounter import Encounter
+from .resources.immunization import Immunization
+from .resources.location import Location
+from .resources.medicationadministration import MedicationAdministration
+from .resources.medicationstatement import MedicationStatement
+from .resources.observation import Observation
+from .resources.organization import Organization
+from .resources.patient import Patient
+from .resources.procedure import Procedure
+from .resources.researchsubject import ResearchSubject
+from .resources.specimen import Specimen
+
+from .ingest import convert_data_to_flat
